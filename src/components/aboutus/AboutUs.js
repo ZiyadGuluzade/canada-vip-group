07b8046421eee45bdac9b174c2b206b78a3fa183
Z.Guluzade <ziyad_guluzade@outlook.com>
2020-07-20 17:24:21 -0400

    added google maps embed api

--- src/components/aboutus/AboutUs.js
+++ src/components/aboutus/AboutUs.js
@@ -10,9 +10,9 @@ class AboutUs extends React.Component {
             <h2 className='about-us-title'>Our Company</h2>
             <p className='about-text'>We connect the best proffessionals in the industry to provide you best service</p>
             <div className='about-inner-div'>
-            <p className='about-us-text'>AGV Health was found on the idea of giving to everybody looking for psychotherapy and other restorative medications within the most productive and successful way. Our group comprises of authorized and experienced restorative professionals, advisors and social specialists who prioritize most elevated level of polished skill and client fulfillment.
-            We offer: Direction, appraisal and medications performed by bilingual, authorized Enlisted Psychotherapists specializing in numerous ranges of hone counting but not restricted to working with newcomers, outcasts, youth, children (age 5+), families, casualties, members of Innate and The Primary Countries populace. *FREE and Secured Administrations Accessible, it would be ideal if you contact for details. More grounded and more established applications for those enduring from mental conditions with a substantial and proficient psychotherapy reports.
-            We offer psychotherapy and related administrations to those going through Migration Law, H&C, Family Law, Business Law, Human Rights, Criminal Law, Individual Harm forms, and private clients. Our hone incorporates but isn't restricted to Cognitive-Behavioural Treatment (CBT), Argumentative Behavioral Treatment (DBT), Inner Family Frameworks Treatment (Uncertainties), Connected Behavioral Treatment (ABA), Play Treatment, Craftsmanship Treatment, Eye Development Desensitization and Reprocessing Treatment (EMDR), Mindfulness-Based Cognitive Treatment (MBCT), Therapy, Person-Centered Treatment (Rogerian Treatment), Cognitive Appraisal, Psychometric Evaluation and Psycho-Educational Evaluation.
+              <p>   sCanada VIP Group was founded on the idea of bringing together best proffessionals from various industries into on1 place. Our group determined to provide and maintain 
+            We offer best service in: real estate, house renovation, healthcare, car services & rentals, immigration services etc.
+            
               </p>   
             </div>
           </div>
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -7,14 +7,21 @@ class Contact extends React.Component {
       <React.Fragment>
         <section id='contact'>
           <h2 className='contacts-title'>Contact Information</h2>
-          <p className=''><span className='addresses'>Address:</span> 7181 Yonge Street, Thornhill, ON /Shops on Yonge</p>
-          <p><span className='addresses'>Postal Code:</span> L3T 0C7</p>
-              <p><span className='addresses'>Phone:</span> 416 731 42 42</p>
-              <p><span className='addresses'>Email:</span> info@canadavipgroup.com</p>
+          <p className='contact-info'>Please contact us for question and additionl information</p>
+          <p className='contact-info'><span className='addresses'><i class="fas fa-search-location"></i>
+          </span> 7181 Yonge Street, Thornhill, ON /Shops on Yonge,L3T 0C7</p>
+              <p><span className='addresses'><i class="fas fa-phone"></i> 416 731 42 42</span>   <span className='addresses'><i class="far fa-envelope"></i> info@canadavipgroup.com</span></p>
+              {/* AIzaSyAKt6PhyNV0hiP0aNIfxQmuRFjloDZpYgw */}
           <div className='contacts'>
             <div className='address'>
-              
-              
+            <iframe
+              width="500"
+              height="450"
+              frameborder="0" style={{border: '1px'}}
+              src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAKt6PhyNV0hiP0aNIfxQmuRFjloDZpYgw 
+              &q=Shops+on+Yonge,Toronto+ON" allowFullScreen>
+            </iframe>
+            
             </div>
             <div className='email-form'>
 
